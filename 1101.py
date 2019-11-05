a59c751b638c490d8c916aad744a01cd91220c32
Tununung <37055373+Tununung@users.noreply.github.com>
2019-11-05 11:35:03 +0800

    ff

--- 1101.py
+++ 1101.py
@@ -1,3 +1,17 @@
 from datetime import datetime
 d1 = datetime.now()
+<<<<<<< HEAD
 print(d1)
+=======
+print(d1,d1)
+<<<<<<< HEAD
+<<<<<<< HEAD
+ssss
+=======
+SDFDFDS
+DSFDFD
+FDSFDSF
+>>>>>>> 4b887a6dd8d185c7551a0931978c5f32166db2de
+=======
+>>>>>>> parent of 31b6bab... 1102test
+>>>>>>> parent of 27e62cc... test
